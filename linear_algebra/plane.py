01661b737784feb641d9e01c59f81825098f963e
Mark Schutt <moschutt@gmail.com>
2017-06-06 09:03:37 -0400

    Check in

--- linear_algebra/plane.py
+++ linear_algebra/plane.py
@@ -132,21 +132,21 @@ class MyDecimal(Decimal):
     def is_near_zero(self, eps=1e-10):
         return abs(self) < eps
 
-p1 = Plane(Vector([-0.412, 3.806, 0.728]), -3.46)
-p2 = Plane(Vector([1.03, -9.515, -1.82]), 8.65)
-
-print('p1 == p2: ', p1 == p2)
-print('p1 // p2: ', p1.is_parallel_to(p2))
-
-p3 = Plane(Vector([2.611, 5.528, 0.283]), 4.6)
-p4 = Plane(Vector([7.715, 8.306, 5.342]), 3.76)
-
-print('p3 == p4: ', p3 == p4)
-print('p3 // p4: ', p3.is_parallel_to(p4))
-
-p5 = Plane(Vector([-7.926, 8.625, -7.212]), -7.952)
-p6 = Plane(Vector([-2.642, 2.875, -2.404]), -2.443)
-
-print('p5 == p6: ', p5 == p6)
-print('p5 // p5: ', p5.is_parallel_to(p6))
+#p1 = Plane(Vector([-0.412, 3.806, 0.728]), -3.46)
+#p2 = Plane(Vector([1.03, -9.515, -1.82]), 8.65)
+#
+#print('p1 == p2: ', p1 == p2)
+#print('p1 // p2: ', p1.is_parallel_to(p2))
+#
+#p3 = Plane(Vector([2.611, 5.528, 0.283]), 4.6)
+#p4 = Plane(Vector([7.715, 8.306, 5.342]), 3.76)
+#
+#print('p3 == p4: ', p3 == p4)
+#print('p3 // p4: ', p3.is_parallel_to(p4))
+#
+#p5 = Plane(Vector([-7.926, 8.625, -7.212]), -7.952)
+#p6 = Plane(Vector([-2.642, 2.875, -2.404]), -2.443)
+#
+#print('p5 == p6: ', p5 == p6)
+#print('p5 // p5: ', p5.is_parallel_to(p6))
     
